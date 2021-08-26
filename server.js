30e796e785d028d07254a201e68927bcaec8ecb5
HariKrishna-28 <harikrishnac2002@gmail.com>
2021-08-26 17:02:51 +0530

    added heroku config

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 // const io = require('socket.io')(5000)
-const app = express();
+const express = require("express")
 const cors = require("cors")
-const path = require("path")
+const path = require('path')
 const baseUrl = "https://chat-app-28.herokuapp.com"
 
 
-const io = require("socket.io")(5000, {
+const app = express();
+
+const io = require("socket.io")(process.env.PORT || 5000, {
     cors: {
         origin: { baseUrl }
     }
 });
 
-//  heroku config
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
 
 io.on('connection', socket => {
     const id = socket.handshake.query.id
@@ -32,3 +28,11 @@ io.on('connection', socket => {
         })
     })
 })
+
+
+// heroku config
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
